21416e4d1ec762b22862f7a04ef7e0b2c1575cca
Gabriel <gabriel9938@gmail.com>
2023-11-21 12:49:23 -0300

    add sign

---
+++ dist/WaxUser.js
@@ -0,0 +1,115 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WaxUser = void 0;
+const universal_authenticator_library_1 = require("universal-authenticator-library");
+const UALWaxError_1 = require("./UALWaxError");
+// import {
+//   APIClient,
+//   PackedTransaction,
+//   SignedTransaction,
+// } from "@greymass/eosio";
+class WaxUser extends universal_authenticator_library_1.User {
+    constructor(chain, userAccount, pubKeys, isTemp, wax) {
+        super();
+        this.accountName = userAccount;
+        this.requestPermission = "active";
+        this.pubKeys = pubKeys;
+        this.isTemp = isTemp;
+        this.chain = chain;
+        this.wax = wax;
+        // compatible features
+        this.api = wax.api;
+        this.rpc = wax.api && wax.api.rpc;
+    }
+    /**
+     * @param transaction  The transaction to be signed (a object that matches the RpcAPI structure).
+     * @param options  Options for tapos fields
+     */
+    async signTransaction(transaction, options) {
+        try {
+            const account = await this.wax.login();
+            if (account !== this.accountName) {
+                throw new Error("Account does not match the requested permission");
+            }
+            else {
+                this.api = this.wax.api;
+                this.rpc = this.wax.api.rpc;
+            }
+            const completedTransaction = await this.wax.api.transact(transaction, options);
+            console.log("options: ", options);
+            if (options.broadcast === false) {
+                let completedTransaction = await this.wax.api.transact(transaction, options);
+                return this.returnEosjsTransaction(options.broadcast !== false, completedTransaction);
+            }
+            else {
+                options.broadcast = false;
+                let completedTransaction = await this.wax.api.transact(transaction, options);
+                console.log("completedTransaction: ", completedTransaction);
+                console.log("completedTransaction: ", completedTransaction.signatures);
+                var data = {
+                    signatures: completedTransaction.signatures,
+                    compression: 0,
+                    serializedContextFreeData: undefined,
+                    serializedTransaction: completedTransaction.serializedTransaction,
+                };
+                console.log("Data: ", data);
+                var retries = 3;
+                var retry = false;
+                try {
+                    completedTransaction = await this.wax.api.rpc.send_transaction(data);
+                    console.log("completed: ", completedTransaction);
+                    return this.returnEosjsTransaction(true, completedTransaction);
+                }
+                catch (e) {
+                    const message = "api.rpc.send_transaction FAILED";
+                    console.log("Error: ", message);
+                    retry = true;
+                }
+                if (retry) {
+                    var res = {};
+                    var completed = false;
+                    while (retries > 0) {
+                        try {
+                            res = await this.wax.api.rpc.send_transaction(data);
+                            completed = true;
+                            console.log(res);
+                        }
+                        catch (e) {
+                            console.log(JSON.stringify(e));
+                        }
+                        // check for completed - need to check actual returned messages
+                        if (completed) {
+                            return this.returnEosjsTransaction(true, res);
+                        }
+                        retries--;
+                        new Promise((resolve) => setTimeout(resolve, 300));
+                    }
+                    throw new UALWaxError_1.UALWaxError("Transaction failed because of ms limitation please retry", universal_authenticator_library_1.UALErrorType.Signing, completedTransaction);
+                }
+            }
+            return this.returnEosjsTransaction(options.broadcast !== false, completedTransaction);
+        }
+        catch (e) {
+            throw new UALWaxError_1.UALWaxError(e.message ? e.message : "Unable to sign transaction", universal_authenticator_library_1.UALErrorType.Signing, e);
+        }
+    }
+    async signArbitrary() {
+        throw new UALWaxError_1.UALWaxError("WAX Cloud Wallet does not currently support signArbitrary", universal_authenticator_library_1.UALErrorType.Unsupported, null);
+    }
+    async verifyKeyOwnership() {
+        throw new UALWaxError_1.UALWaxError("WAX Cloud Wallet does not currently support verifyKeyOwnership", universal_authenticator_library_1.UALErrorType.Unsupported, null);
+    }
+    async getAccountName() {
+        return this.accountName;
+    }
+    async getChainId() {
+        return this.chain.chainId;
+    }
+    async getKeys() {
+        return this.pubKeys;
+    }
+    async getIsTemp() {
+        return this.isTemp;
+    }
+}
+exports.WaxUser = WaxUser;
--- src/WaxUser.ts
+++ src/WaxUser.ts
@@ -1,94 +1,183 @@
-import {Chain, SignTransactionResponse, User, UALErrorType} from 'universal-authenticator-library'
-import {WaxJS} from "@waxio/waxjs/dist"
-import {UALWaxError} from "./UALWaxError";
+import {
+  Chain,
+  SignTransactionResponse,
+  User,
+  UALErrorType,
+} from "universal-authenticator-library";
+import { WaxJS } from "@waxio/waxjs/dist";
+import { UALWaxError } from "./UALWaxError";
+
+// import {
+//   APIClient,
+//   PackedTransaction,
+//   SignedTransaction,
+// } from "@greymass/eosio";
 
 export class WaxUser extends User {
-    public readonly accountName: string;
-    public readonly requestPermission: string;
+  public readonly accountName: string;
+  public readonly requestPermission: string;
 
-    private readonly pubKeys: string[];
-    private readonly isTemp: boolean;
-    private readonly wax: WaxJS;
-    private readonly chain: Chain;
+  private readonly pubKeys: string[];
+  private readonly isTemp: boolean;
+  private readonly wax: WaxJS;
+  private readonly chain: Chain;
 
-    public api: any;
-    public rpc: any;
+  public api: any;
+  public rpc: any;
 
-    constructor(
-      chain: Chain,
-      userAccount: string,
-      pubKeys: string[],
-      isTemp: boolean,
-      wax: WaxJS
-    ) {
-        super();
+  constructor(
+    chain: Chain,
+    userAccount: string,
+    pubKeys: string[],
+    isTemp: boolean,
+    wax: WaxJS
+  ) {
+    super();
 
-        this.accountName = userAccount;
-        this.requestPermission = 'active';
-        this.pubKeys = pubKeys;
-        this.isTemp = isTemp;
+    this.accountName = userAccount;
+    this.requestPermission = "active";
+    this.pubKeys = pubKeys;
+    this.isTemp = isTemp;
 
-        this.chain = chain;
-        this.wax = wax;
+    this.chain = chain;
+    this.wax = wax;
 
-        // compatible features
-        this.api = wax.api;
-        this.rpc = wax.api && wax.api.rpc;
-    }
+    // compatible features
+    this.api = wax.api;
+    this.rpc = wax.api && wax.api.rpc;
+  }
 
-    /**
-     * @param transaction  The transaction to be signed (a object that matches the RpcAPI structure).
-     * @param options  Options for tapos fields
-     */
-    async signTransaction(transaction: any, options: any): Promise<SignTransactionResponse> {
-        try {
-            const account = await this.wax.login();
+  /**
+   * @param transaction  The transaction to be signed (a object that matches the RpcAPI structure).
+   * @param options  Options for tapos fields
+   */
+  async signTransaction(
+    transaction: any,
+    options: any
+  ): Promise<SignTransactionResponse> {
+    try {
+      const account = await this.wax.login();
 
-            if (account !== this.accountName) {
-                throw new Error('Account does not match the requested permission');
-            } else {
-                this.api = this.wax.api;
-                this.rpc = this.wax.api.rpc;
-            }
+      if (account !== this.accountName) {
+        throw new Error("Account does not match the requested permission");
+      } else {
+        this.api = this.wax.api;
+        this.rpc = this.wax.api.rpc;
+      }
 
-            const completedTransaction = await this.wax.api.transact(transaction, options);
+      const completedTransaction = await this.wax.api.transact(
+        transaction,
+        options
+      );
 
-            return this.returnEosjsTransaction(options.broadcast !== false, completedTransaction);
-        } catch (e) {
-            throw new UALWaxError(
-                e.message ? e.message : 'Unable to sign transaction',
-                UALErrorType.Signing, e
-            );
-        }
-    }
+      console.log("options: ", options);
 
-    async signArbitrary(): Promise<string> {
-        throw new UALWaxError(
-            'WAX Cloud Wallet does not currently support signArbitrary',
-            UALErrorType.Unsupported, null
+      if (options.broadcast === false) {
+        let completedTransaction: any = await this.wax.api.transact(
+          transaction,
+          options
         );
-    }
+        return this.returnEosjsTransaction(
+          options.broadcast !== false,
+          completedTransaction
+        );
+      } else {
+        options.broadcast = false;
+        let completedTransaction: any = await this.wax.api.transact(
+          transaction,
+          options
+        );
+        console.log("completedTransaction: ", completedTransaction);
+        console.log("completedTransaction: ", completedTransaction.signatures);
 
-    async verifyKeyOwnership(): Promise<boolean> {
+        var data = {
+          signatures: completedTransaction.signatures,
+          compression: 0,
+          serializedContextFreeData: undefined,
+          serializedTransaction: completedTransaction.serializedTransaction,
+        };
+
+        console.log("Data: ", data);
+
+        var retries = 3;
+        var retry = false;
+        try {
+          completedTransaction = await this.wax.api.rpc.send_transaction(data);
+          console.log("completed: ", completedTransaction);
+          return this.returnEosjsTransaction(true, completedTransaction);
+        } catch (e) {
+          const message = "api.rpc.send_transaction FAILED";
+          console.log("Error: ", message);
+          retry = true;
+        }
+        if (retry) {
+          var res: any = {};
+          var completed = false;
+          while (retries > 0) {
+            try {
+              res = await this.wax.api.rpc.send_transaction(data);
+              completed = true;
+              console.log(res);
+            } catch (e) {
+              console.log(JSON.stringify(e));
+            }
+            // check for completed - need to check actual returned messages
+            if (completed) {
+              return this.returnEosjsTransaction(true, res);
+            }
+            retries--;
+            new Promise((resolve) => setTimeout(resolve, 300));
+          }
+          throw new UALWaxError(
+            "Transaction failed because of ms limitation please retry",
+            UALErrorType.Signing,
+            completedTransaction
+          );
+        }
+      }
+
+      return this.returnEosjsTransaction(
+        options.broadcast !== false,
+        completedTransaction
+      );
+    } catch (e) {
       throw new UALWaxError(
-          'WAX Cloud Wallet does not currently support verifyKeyOwnership',
-          UALErrorType.Unsupported, null
+        e.message ? e.message : "Unable to sign transaction",
+        UALErrorType.Signing,
+        e
       );
     }
+  }
 
-    async getAccountName(): Promise<string> {
-        return this.accountName;
-    }
+  async signArbitrary(): Promise<string> {
+    throw new UALWaxError(
+      "WAX Cloud Wallet does not currently support signArbitrary",
+      UALErrorType.Unsupported,
+      null
+    );
+  }
 
-    async getChainId(): Promise<string> {
-        return this.chain.chainId;
-    }
+  async verifyKeyOwnership(): Promise<boolean> {
+    throw new UALWaxError(
+      "WAX Cloud Wallet does not currently support verifyKeyOwnership",
+      UALErrorType.Unsupported,
+      null
+    );
+  }
 
-    async getKeys() {
-        return this.pubKeys;
-    }
+  async getAccountName(): Promise<string> {
+    return this.accountName;
+  }
 
-    async getIsTemp() {
-        return this.isTemp;
-    }
+  async getChainId(): Promise<string> {
+    return this.chain.chainId;
+  }
+
+  async getKeys() {
+    return this.pubKeys;
+  }
+
+  async getIsTemp() {
+    return this.isTemp;
+  }
 }
